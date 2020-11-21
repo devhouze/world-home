bc26180faee67baa2f2171a2cd8d4625a0df2d18
infodevsujit <infodevsujit@gmail.com>
2020-11-21 14:46:48 +0530

    add-title

--- include/header.php
+++ include/header.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
-
+<title>World Homes</title>
 </head>
 
 <body>
