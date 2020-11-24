50b9e2d81b699b4bdc698071f21fd79c3d048c88
infodevsujit <infodevsujit@gmail.com>
2020-11-24 18:50:45 +0530

    finish-work

--- thankyou.php
+++ thankyou.php
@@ -1,7 +1,7 @@
 <?php include 'include/header.php'; ?>
 <style>
     .header-thanks {
-        background-color: #F8EABE;
+        background: white;
     }
 
     header {
@@ -10,6 +10,10 @@
     .menu
     {
         display: none !important;
+    }
+    footer p
+    {
+        margin-top: 30px;
     }
 </style>
 <div class="header-thanks py-4 ">
@@ -24,17 +28,22 @@
     </div>
 
 </div>
-<div class="thankyou min-vh-100 d-flex flex-fill">
+<div class="thankyou min-vh-100 d-flex flex-fill position-relative">
+    <div class="circle-th"><img src="images/circle.svg"  alt=""></div>
+    <div class="clock-th"><img src="images/clock.svg"  alt=""></div>
     <div class="container d-flex flex-fill">
         <div class="row d-flex flex-fill">
             <div class="col-md-12 d-flex flex-fill align-items-center justify-content-center flex-column">
-                <figure class=""><img src="images/ok.svg" alt=""></figure>
-                <h1 class="pr-font mb-3 mt-3">Thank You!</h1>
-                <h2>An Expert will call you Soon.</h2>
-                <a href="index.php">Go Back to Homepage</a>
-
+                <div class="umbr"><img src="images/umbrala.svg" alt=""></div>
+                <figure class=""><img src="images/topreb.svg" alt=""></figure>
+                <h1 class="pr-font mb-5 mt-3">Thank <span>You!</span></h1>
+                <h2 class="pr-font">An Expert will call you Soon.</h2>
+                <a href="index.php" class="pr-font">Go Back to Homepage</a>
+                <figure class="mt-4"><img src="images/bottomreb.svg" alt=""></figure>
             </div>
         </div>
     </div>
+    
 </div>
+<img src="images/thanks-bg.svg" class="d-table mx-auto mb-md-4" alt="">
 <?php include 'include/footer.php'; ?>
